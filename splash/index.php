a5c7b7137c0eecb2724d1942a057a5a8c832b7e6
Peter DuCharme <peter.ducharme@nautil.us>
2015-08-07 18:10:34 -0400

    front

---
+++ splash/index.php
@@ -0,0 +1,128 @@
+<?php
+$d = json_decode(file_get_contents("store.json"));
+//print_r($d);
+?>
+<!DOCTYPE html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+  <title>ICON The Illustration Conference</title>
+  <meta name="description" content="Website for the Illustration Conference, ICON 9 taking place in Austin, Texas July 6-9, 2016">
+  <meta name="keywords" content="ICON, Illustration, Design, Conference, Austin">
+  <meta name="author" content="The Illustration Conference">
+  <meta name="viewport" content="width=960, minimal-ui">
+<!--<script src="//use.typekit.net/zyr2cee.js"></script>-->
+<!--<script>try{Typekit.load({ async: true });}catch(e){}</script>-->
+
+ <link rel="stylesheet" href="/css/main.css">
+ </head>
+<body>
+
+<!--
+<span class="icon_head_a">1</span>
+<span class="icon_head_a">2</span>
+<i class="fa fa-twitter"></i>
+<i class="fa fa-instagram"></i>
+<i class="fa fa-tumblr"></i>
+ -->
+<div id="texas" class="fpanel">
+
+    <img id="icon_head_a" src="/img/logo_900.png" alt="ICON9" title="ICON9"/>
+
+
+
+<div id="savedate">Save the date</div>
+<div id="date">July 6-9, 2016<br/>Austin, TX</div>
+<div id="hotel">Austin Hilton Hotel</div>
+<a style="text-decoration:none;" href=""><div id="emailme">Email Me Updates</div></a>
+<div id="pan-one-space"></div>
+<div id="tickreg">Ticket registration for ICON9 opens in the Fall. Stay tuned for announcements.</div>
+
+
+
+<ul class="social">
+<li><a href="#" title="Twitter"><i class="fa fa-twitter"></i></a></li>
+<li><a href="#" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+<li><a href="#" title="Tumblr"><i class="fa fa-tumblr"></i></a></li>
+</ul>
+
+</div>
+
+
+<div id="watercolor" class="fpanel">
+
+    <div class="box-header">Contact</div>
+
+    <div class="emreg">
+    <div id="join-email" style="float: none;">Join our email list to receive updates on the conference.</div>
+
+    <div class="con-inpts">
+        <label>First Name</label>
+        <input type="text" name="asd">
+    </div>
+    <div class="con-inpts">
+        <label>Last Lame</label>
+        <input type="text" name="asd">
+    </div>
+    <div class="con-inpts">
+        <label>Email</label>
+        <input type="text" name="asd">
+    </div>
+
+        <a style="text-decoration:none;" href=""><div id="subcon">Submit</div></a>
+<div class="conmsg">
+For sponsorship and general inquiries,<br/>
+Contact Mark Heflin: <a href="mailto:office@illustrationconference.org?subject=Website%20Contact">office@illustrationconference.org</a>
+</div>
+</div>
+
+</div>
+
+
+<div id="news" class="fpanel">
+    <div class="box-header">News</div>
+
+    <div class="cntrclmn">
+        <div id="igrm" class="fl socboxes">
+            <i class="fa fa-instagram"></i> @ICONconference<br/>
+
+            <img width="424" src="<?= $d->inst->imgsrc?>" />
+
+        </div>
+        <div id="twt" class="fl socboxes">
+            <i class="fa fa-twitter"></i> @ICONconference<br/>
+
+
+<?= $d->twttr->text?>
+
+        </div>
+
+
+        <div id="tmb" class="fl socboxes">
+            <i class="fa fa-tumblr"></i> ICON Tumblr<br/>
+
+            <?= $d->tmblr->title?>
+
+
+        </div>
+
+
+    </div>
+
+</div>
+
+
+<div id="what" class="fpanel">
+What
+</div>
+
+
+<div id="footer" class="fpanel">
+Footer
+</div>
+
+
+
+
+</body>
---
+++ splash/social.php
@@ -0,0 +1,55 @@
+<?php
+
+
+$mod = filemtime(__DIR__ . "/store.json");
+$now = strtotime("now");
+print $now - $mod;
+
+if(($now - $mod) < (120*3600)){die();}
+
+$old = json_decode(file_get_contents(__DIR__ . "/store.json"));
+
+
+
+$dataFile = file_get_contents("https://instagram.com/iconconference/");
+$start_position = strpos( $dataFile ,'{"static_root"' ); // start position
+$trimmed_before = trim( substr($dataFile, $start_position) ); // trim preceding content
+$end_position = strpos( $trimmed_before, '</script>'); // end position
+$trimmed = trim( substr( $trimmed_before, 0, $end_position) ); // trim content
+$jsondata = substr( $trimmed, 0, -1); // remove extra trailing ";"
+
+$x = (json_decode($jsondata));
+
+
+$firstentry = $x->entry_data->ProfilePage[0]->user->media->nodes[0]->code;
+$old->inst->url = (isset($firstentry->code))?"https://instagram.com/p/".$firstentry->code:$old->inst->url;
+$old->inst->cap = (isset($firstentry->caption))?$firstentry->caption:$old->inst->cap;
+$old->inst->imgsrc = (isset($firstentry->display_src))?$firstentry->display_src:$old->inst->imgsrc;
+
+
+$xml = simplexml_load_file("http://theillustrationconference.tumblr.com/rss");
+
+$test="";$test = (string)$xml->channel->item[0]->title;
+$old->tmblr->title = (strlen($test) > 0)?$test:$old->tmblr->title;
+
+$test="";$test = (string) $xml->channel->item[0]->link;
+$old->tmblr->url = (strlen($test) > 0)?$test:$old->tmblr->url;
+
+
+
+
+$con = file_get_contents("https://twitter.com/iconconference");
+preg_match_all('/js-tweet-text tweet-text(.*?)>(.*?)<\/p>/',$con,$out);
+$test="";$test = $out[2][0];
+$old->twttr->text = (strlen($test) > 0)?$test:$old->twttr->text;
+
+$content = json_encode($old);
+$fp = fopen(__DIR__ . "/store.json","wb");
+fwrite($fp,$content);
+fclose($fp);
+
+
+
+
+
+?>
